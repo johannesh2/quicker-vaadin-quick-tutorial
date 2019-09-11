20b911949ca1ff075a33d464378cb0f6d59b0711
Johannes Häyry <johannes.hayry@vaadin.com>
2019-09-11 15:13:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Run application using mvn spring-boot:run or directly running Application class from your IDE.
+
+Open http://localhost:8080/ in browser.
---
+++ src/main/java/com/vaadin/example/MainView.java
@@ -0,0 +1,37 @@
+package com.vaadin.example;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        VerticalLayout todosList = new VerticalLayout(); // (1)
+        TextField taskField = new TextField(); // (2)
+        Button addButton = new Button("Add"); // (3)
+        addButton.addClickShortcut(Key.ENTER);
+        addButton.addClickListener(click -> {
+            // (4)
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todosList.add(checkbox);
+        });
+
+        add( // (5)
+             new H1("Vaadin Todo"),
+             todosList,
+             new HorizontalLayout(
+                 taskField,
+                 addButton
+             )
+        );
+    }
+}
+
